697dfa4132fbd2a53ba0f0eb657800e50f68399a
singh957523 <adityabhadouriya546@gmail.com>
2026-06-10 20:13:02 +0530

    this is lec3

---
+++ lec3.py
@@ -0,0 +1,41 @@
+# # # def calculator():
+# # #     num1 = float(input("Enter first number: "))
+# # #     op = input("Enter operator (+, -, *, /): ")
+# # #     num2 = float(input("Enter second number: "))
+
+# # #     if op == "+":
+# # #         return num1 + num2
+# # #     elif op == "-":
+# # #         return num1 - num2
+# # #     elif op == "*":
+# # #         return num1 * num2
+# # #     elif op == "/":
+# # #         return "Error: Division by zero" if num2 == 0 else num1 / num2
+# # #     else:
+# # #         return "Invalid operator"
+
+# # # print("Result:", calculator())
+
+# # # task2
+
+# # def password():
+# #     password = input("enter your password")
+# #     ch = len(ch)
+# #     if ch>=8:
+# #         print("true")
+# #     else:
+# #         print("invalid pass")
+    
+# # password()
+
+# taske3
+def tempreature():
+ celsius = float(input("enter your value"))
+ if farenhite == (celsius*9/5)+ 32:
+   print("farenhite")
+ else:
+   print("invalid")
+
+tempreature()   
+
+    
